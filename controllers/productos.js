9d62bd433f9d0baa9f30c2bfd5459d7eec14175a
Jean Paul Ferreira <jeanpaul@jepafe.net>
2021-11-23 11:58:40 -0300

    Clase 31

--- controllers/productos.js
+++ controllers/productos.js
@@ -1,6 +1,6 @@
 const { validationResult } = require('express-validator')
 
-const connection = require('./db')
+const connection = require('../db')
 
 const index = (req, res) => {
     connection.query('SELECT id, nombre FROM productos', (error, results) => {
@@ -50,11 +50,46 @@ const store = (req, res) => {
 }
 
 const edit = (req, res) => {
-    res.render('productos/edit')
+    // res.render('productos/edit', { values: {} })
+
+    connection.query('SELECT id, nombre, descripcion FROM productos WHERE id = ?', [ req.params.nro ], (error, result) => {
+        if (error) {
+            throw error
+        }
+    
+        if (result.length > 0) {
+            res.render('productos/edit', { values: {}, producto: result[0] })
+        } else {
+            res.send('No existe el producto')
+        }
+    })
 }
 
 const update = (req, res) => {
-    res.send('actualizando...')
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.render('productos/edit', { values: req.body, errors: errors.array(), producto: {} })
+    } else {
+        connection.query('UPDATE productos SET ? WHERE id = ?', [{ nombre: req.body.nombre, descripcion: req.body.description }, req.body.id ], (error) => {
+            if (error) {
+                throw error
+            }
+    
+            res.redirect('/productos')
+        })
+    }
+}
+
+const destroy = (req, res) => {
+    // res.send('Borrado: ' + req.params.nro)
+
+    connection.query('DELETE FROM productos WHERE id = ?', [ req.params.nro ], (error) => {
+        if (error) {
+            throw error
+        }
+
+        res.redirect('/productos')
+    })
 }
 
 module.exports = {
@@ -63,5 +98,6 @@ module.exports = {
     create,
     store,
     edit,
-    update
+    update,
+    destroy
 }
