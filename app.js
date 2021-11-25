1cc5dd313b5ad55d6a8a0a07aaf8d49525ca712d
Jean Paul Ferreira <jeanpaul@jepafe.net>
2021-11-25 12:18:23 -0300

    Clase 32

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const methodOverride = require('method-override')
+const session = require('express-session')
+
+app.use(session({
+    secret: 'FV*_54V56yG^B_<5',
+    resave: false,
+    saveUninitialized: false
+}))
 
 app.set('view engine', 'ejs')
 app.use(expressLayouts)
@@ -12,6 +19,21 @@ app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
 
+const isLogin = (req, res, next) => {
+    if (!req.session.user_id && req.url != '/login' && req.url != '/register'&& req.url != '/contacto') {
+        res.redirect('/login')
+    }
+
+    next()
+}
+
+app.use(isLogin)
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.use('/', require('./routes/auth'))
 app.use('/', require('./routes/productos'))
 app.use('/', require('./routes/contacto'))
 
--- codo_a_codo.sql
+++ codo_a_codo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 16-11-2021 a las 15:48:15
+-- Tiempo de generación: 25-11-2021 a las 15:06:16
 -- Versión del servidor: 10.6.4-MariaDB
 -- Versión de PHP: 7.4.19
 
@@ -42,6 +42,18 @@ INSERT INTO `productos` (`id`, `nombre`, `descripcion`) VALUES
 (2, 'Producto Nro 2', NULL),
 (3, 'Producto desde la DB', 'Aliquam ut ipsum nec leo venenatis lacinia quis quis purus. Maecenas molestie suscipit nisi in congue. Nullam facilisis venenatis lectus a eleifend. Curabitur dignissim leo a mauris ultrices bibendum. Donec ullamcorper, nisl a tempor egestas, elit libero aliquam tellus, eget vehicula risus nisl eget lorem. Aenean pellentesque elit viverra, ultricies justo nec, dignissim magna. Suspendisse condimentum libero quis nisi luctus, quis egestas quam posuere. Duis semper sapien non felis pulvinar venenatis. Nam mi tortor, rutrum tempor sollicitudin viverra, pellentesque et sapien.\n\nSed sollicitudin finibus vehicula. Donec nisl risus, malesuada id sem ut, euismod ultrices arcu. Nulla venenatis ultricies semper. Aliquam cursus eleifend mi, non rhoncus dui tempus tempus. Vestibulum at dapibus felis. Fusce laoreet, ex ut malesuada pellentesque, est purus commodo lorem, ac tempus elit enim nec nisl. Etiam nec pretium elit, non malesuada ex. In tristique, enim eget vehicula sodales, ante magna porta tellus, non porttitor mauris augue sed est. Praesent fringilla, nibh quis congue auctor, risus ex fermentum lorem, non imperdiet tortor leo non lacus. Quisque imperdiet nec dui ut ullamcorper. Nam gravida, tellus sed malesuada dignissim, orci mauris sagittis odio, vitae interdum est nisi vel odio. Etiam non molestie nisl. Fusce elementum scelerisque metus quis venenatis. Ut finibus, odio vel dictum ultricies, tellus eros posuere risus, sit amet auctor arcu risus sed nibh. Fusce consectetur, libero tempor pulvinar tincidunt, arcu turpis pharetra tellus, quis ultricies ligula purus quis felis. In interdum purus a magna pretium, in scelerisque felis mollis.');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Índices para tablas volcadas
 --
@@ -53,6 +65,13 @@ ALTER TABLE `productos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -61,6 +80,12 @@ ALTER TABLE `productos`
 --
 ALTER TABLE `productos`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
